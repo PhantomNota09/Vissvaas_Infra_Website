5f18bcd2226ee0fcae19cd70f75978a8f3a8f735
PhantomNota09 <srayala5@asu.edu>
2024-08-04 05:03:59 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import ContactPage from './ContactPage';
 import ProjectPage from './ProjectPage';
 import ShanmukaaPrides from './ShanmukaaPrides';
 import KuberaaPrides from './KuberaaPrides';
 
-// Import other components/pages like Projects, Contact, etc.
-
 function App() {
     return (
-        <Router>
-            <div className="App">
-                {/* Header or Navigation component can be added here */}
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/projects" element={<ProjectPage />} />
-                    <Route path="/contact" element={<ContactPage />} />
-                    <Route path="/projects/shanmukaa-prides" element={<ShanmukaaPrides />} />
-                    <Route path="/projects/kuberaa-prides" element={<KuberaaPrides />} />
-
-                    {/* Add the ContactPage route */}
-                    {/* Add routes for other pages here */}
-                    {/* Example: <Route path="/projects" element={<Projects />} /> */}
-                    {/* Example: <Route path="/contact" element={<Contact />} /> */}
-                </Routes>
-                {/* Footer component can be added here */}
-            </div>
-        </Router>
+        <div className="App">
+            {/* Header or Navigation component can be added here */}
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/projects" element={<ProjectPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/projects/shanmukaa-prides" element={<ShanmukaaPrides />} />
+                <Route path="/projects/kuberaa-prides" element={<KuberaaPrides />} />
+            </Routes>
+            {/* Footer component can be added here */}
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'; // Ensure this file exists or remove if not needed
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
